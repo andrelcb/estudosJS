80a3aed665b6c5fdf52f288600298f2d9fc6c2b7
Andre Leonardo Correa Barbosa <andre.barbosa@avantibrpremium.local>
2019-03-27 14:36:51 -0300

    aula veiculo

---
+++ dist/app3.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Carro_1 = require("./Carro");
+var Moto_1 = require("./Moto");
+var carro = new Carro_1.default("Fiat Uno", 10);
+var moto = new Moto_1.default();
+console.log(moto);
+console.log(carro);
